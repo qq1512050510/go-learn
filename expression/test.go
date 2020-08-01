ebd946639f933f98a7c352937eb9fa92524aa030
jiangyupeng <765819328@qq.com>
2020-08-01 23:16:52 +0800

    git basic grammar

---
+++ expression/test.go
@@ -0,0 +1,13 @@
+package expression
+
+func main(){
+	x :=100
+	println(x)
+	if x>0 {
+		println(x)
+	}else if x<0{
+		println(-x)
+	}else{
+		println("0")
+	}
+}
--- hello.go
+++ hello.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
-
+//import "fmt"
+import (
+	"fmt"
+	) 
 func main(){
+	var a string = "Test"
+	var b bool
+	var c int
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 	fmt.Println("hello,go")
+	var s string
+	fmt.Printf("%v %v %q\n",a,b,s)
+	var intVal int 
+
+	//intVal :=1 // 这时候会产生编译错误
+
+	intVal,intVal1 := 1,2 // 此时不会产生编译错误，因为有声明新的变量，因为 := 是一个声明语句
+	fmt.Printf("%v %v",intVal,intVal1)
+	f := "Runoob" // var f string = "Runoob"
+
+	var m,n int = 1,2
+
+	fmt.Println(f)
+	fmt.Printf("%v和%v\n",m,n)
+	m,n = n,m
+	fmt.Printf("%v和%v\n",m,n)
+
+	println("Hello World!")
+
 }
