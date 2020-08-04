c0ac4ba92f2ee97057daa84f4a93f9af5b5ef285
jiangyupeng <765819328@qq.com>
2020-08-04 10:38:23 +0800

    commit

--- hello.go
+++ hello.go
@@ -32,5 +32,37 @@ func main(){
 	println("Hello World!")
 
 	println("提交测试")
+	const(
+		aa = iota
+		bb
+		cc
+		ee
+		dd = "ha"
+		ff = 100
+		gg
+		hh = iota
+		ii
+	)
+	fmt.Println(aa,bb,cc,dd,ee,ff,gg,hh,ii)
+
+	/*
+	select case
+	*/
+	var c1, c2, c3 chan int
+	var i1, i2 int
+	select {
+	case i1 = <-c1:
+		fmt.Printf("received",i1,"from c1\n")
+	case c2<- i2:
+		fmt.Printf("sent",i2,"to c2\n")
+	case i3, ok:=(<-c3):// same as: i3, ok := <-c3
+		if ok {
+			fmt.Printf("received",i3," from c3\n")
+		}else{
+			fmt.Printf("c3 is closed\n")
+		}
+	default:
+		fmt.Printf("no communication\n")
+	}
 
 }
